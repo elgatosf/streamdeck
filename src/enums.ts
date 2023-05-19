47fd303e76f50fcc6f19708b250956a10208c567
Richard Herman <geekyeggo@users.noreply.github.com>
2023-05-19 18:25:06 +0100

    docs: add missing docs to enums

--- src/enums.ts
+++ src/enums.ts
@@ -1,16 +1,64 @@
+/**
+ * Stream Deck devices.
+ */
 export enum DeviceType {
+	/**
+	 * Stream Deck, comprising of 15 buttons in a 5x3 layout.
+	 */
 	StreamDeck = 0,
+
+	/**
+	 * Stream Deck Mini, comprising of 6 buttons in a 3x2 layout.
+	 */
 	StreamDeckMini = 1,
+
+	/**
+	 * Stream Deck XL, comprising of 32 buttons in an 8x4 layout.
+	 */
 	StreamDeckXL = 2,
+
+	/**
+	 * Stream Deck Mobile for iOS and Android.
+	 */
 	StreamDeckMobile = 3,
+
+	/**
+	 * Corsair G Keys, buttons available on selected Corsair keyboards.
+	 */
 	CorsairGKeys = 4,
+
+	/**
+	 * Stream Deck Pedal.
+	 */
 	StreamDeckPedal = 5,
+
+	/**
+	 * Corsair Voyager laptop, comprising 10 buttons in a horizontal line above the keyboard.
+	 */
 	CorsairVoyager = 6,
+
+	/**
+	 * Stream Deck+, comprising of 8 buttons in a 4x2 layout and 4 dials with accompanying touch screen.
+	 */
 	StreamDeckPlus = 7
 }
 
+/**
+ * Defines the target, used when sending specific requests to Stream Deck. Applies to `StreamDeck.setImage` and `StreamDeck.setState`
+ */
 export enum Target {
+	/**
+	 * Hardware and software should be updated as part of the request.
+	 */
 	HardwareAndSoftware = 0,
+
+	/**
+	 * Hardware only should be updated as part of the request.
+	 */
 	Hardware = 1,
+
+	/**
+	 * Software only should be updated as part of the request.
+	 */
 	Software = 2
 }
