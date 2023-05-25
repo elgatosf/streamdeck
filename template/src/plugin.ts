d1b3eeb936e87c551368abd3ccde4b13fc008773
Richard Herman <geekyeggo@users.noreply.github.com>
2023-05-25 16:57:20 +0100

    add: basic template

--- template/src/plugin.ts
+++ template/src/plugin.ts
@@ -1,6 +1,26 @@
-// This currently requires npm link whilst we develop.
-import streamDeck from "@elgato/streamdeck";
+import streamDeck, { KeyDownEvent, LogLevel, WillAppearEvent } from "@elgato/streamdeck";
 
-streamDeck.on("didReceiveSettings", (msg) => {
-	console.log(msg.event);
+// Trace logs all messages between the Stream Deck and the plugin; as we aren't using sensitive information, this is fine.
+streamDeck.logger.setLogLevel(LogLevel.TRACE);
+
+// Register a key down event handler that will increment the counter each time it is pressed.
+streamDeck.on("keyDown", async ({ context, payload: { settings } }: KeyDownEvent<CounterSettings>) => {
+	if (settings.count === undefined) {
+		settings.count = 1;
+	} else {
+		settings.count++;
+	}
+
+	streamDeck.setTitle(context, settings.count.toString());
+	streamDeck.setSettings(context, settings);
 });
+
+// When a button appears, set the title to the current count.
+streamDeck.on("willAppear", async ({ context, payload: { settings } }: WillAppearEvent<CounterSettings>) => {
+	streamDeck.setTitle(context, (settings.count ?? 0).toString());
+});
+
+// Settings for the counter action.
+type CounterSettings = {
+	count?: number;
+};
