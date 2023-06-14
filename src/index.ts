1c5d7180927742fd4eca111799fe6ad4443afb7a
Richard Herman <geekyeggo@users.noreply.github.com>
2023-06-14 18:29:00 +0100

    feat: add manifest JSON schema generation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./enums";
 export * from "./events";
 export { LogLevel, default as logger } from "./logger";
+export * from "./manifest";
 export * from "./registration";
 export { default as default } from "./stream-deck";
