e46c784ddcb7c97bbf7541d8c9ee5bd99f6f57de
Richard Herman <1429781+GeekyEggo@users.noreply.github.com>
2023-07-16 19:08:55 +0100

    Add inbound event tests for StreamDeckClient

--- src/connectivity/__mocks__/messages.ts
+++ src/connectivity/__mocks__/messages.ts
@@ -1,4 +1,9 @@
 import * as messages from "../messages";
+import { DeviceType } from "../messages";
+
+const action = "com.elgato.test.one";
+const context = "context123";
+const device = "device123";
 
 /**
  * Mocked {@link messages.ApplicationDidLaunch} message.
@@ -8,4 +13,278 @@ export const applicationDidLaunch: messages.ApplicationDidLaunch = {
 	payload: {
 		application: "notepad.exe"
 	}
+};
+
+/**
+ * Mocked {@link messages.applicationDidTerminate} message.
+ */
+export const applicationDidTerminate: messages.ApplicationDidTerminate = {
+	event: "applicationDidTerminate",
+	payload: {
+		application: "cmd.exe"
+	}
+};
+
+/**
+ * Mocked {@link messages.deviceDidConnect} message.
+ */
+export const deviceDidConnect: messages.DeviceDidConnect = {
+	event: "deviceDidConnect",
+	device,
+	deviceInfo: {
+		name: "Test Device",
+		size: {
+			columns: 8,
+			rows: 4
+		},
+		type: DeviceType.StreamDeckXL
+	}
+};
+
+/**
+ * Mocked {@link messages.deviceDidDisconnect} message.
+ */
+export const deviceDidDisconnect: messages.DeviceDidDisconnect = {
+	event: "deviceDidDisconnect",
+	device
+};
+
+/**
+ * Mocked {@link messages.dialDown} message.
+ */
+export const dialDown: messages.DialDown<Settings> = {
+	event: "dialDown",
+	action,
+	context,
+	device,
+	payload: {
+		controller: "Encoder",
+		coordinates: {
+			column: 3,
+			row: 1
+		},
+		settings: {
+			name: "Elgato"
+		}
+	}
+};
+
+/**
+ * Mocked {@link messages.dialRotate} message.
+ */
+export const dialRotate: messages.DialRotate<Settings> = {
+	event: "dialRotate",
+	action,
+	context,
+	device,
+	payload: {
+		controller: "Encoder",
+		coordinates: {
+			column: 3,
+			row: 1
+		},
+		pressed: true,
+		settings: {
+			name: "Elgato"
+		},
+		ticks: -2
+	}
+};
+
+/**
+ * Mocked {@link messages.dialUp} message.
+ */
+export const dialUp: messages.DialUp<Settings> = {
+	...dialDown,
+	event: "dialUp"
+};
+
+/**
+ * Mocked {@link messages.didReceiveGlobalSettings} message.
+ */
+export const didReceiveGlobalSettings: messages.DidReceiveGlobalSettings<Settings> = {
+	event: "didReceiveGlobalSettings",
+	payload: {
+		settings: {
+			name: "Elgato"
+		}
+	}
+};
+
+/**
+ * Mocked {@link messages.didReceiveSettings} message.
+ */
+export const didReceiveSettings: messages.DidReceiveSettings<Settings> = {
+	action,
+	context,
+	device,
+	event: "didReceiveSettings",
+	payload: {
+		coordinates: {
+			column: 1,
+			row: 3
+		},
+		isInMultiAction: true,
+		settings: {
+			name: "Elgato"
+		}
+	}
+};
+
+/**
+ * Mocked {@link messages.keyDown} message.
+ */
+export const keyDown: messages.KeyDown<Settings> = {
+	action,
+	context,
+	device,
+	event: "keyDown",
+	payload: {
+		coordinates: {
+			column: 2,
+			row: 2
+		},
+		isInMultiAction: false,
+		settings: {
+			name: "Elgato"
+		},
+		state: 1,
+		userDesiredState: 0
+	}
+};
+
+/**
+ * Mocked {@link messages.keyUp} message.
+ */
+export const keyUp: messages.KeyUp<Settings> = {
+	...keyDown,
+	event: "keyUp"
+};
+
+/**
+ * Mocked {@link messages.propertyInspectorDidAppear} message.
+ */
+export const propertyInspectorDidAppear: messages.PropertyInspectorDidAppear = {
+	action,
+	context,
+	device,
+	event: "propertyInspectorDidAppear"
+};
+
+/**
+ * Mocked {@link messages.propertyInspectorDidDisappear} message.
+ */
+export const propertyInspectorDidDisappear: messages.PropertyInspectorDidDisappear = {
+	...propertyInspectorDidAppear,
+	event: "propertyInspectorDidDisappear"
+};
+
+/**
+ * Mocked {@link messages.sendToPlugin} message.
+ */
+export const sendToPlugin: messages.SendToPlugin<Settings> = {
+	action,
+	context,
+	event: "sendToPlugin",
+	payload: {
+		name: "Elgato"
+	}
+};
+
+/**
+ * Mocked {@link messages.systemDidWakeUp} message.
+ */
+export const systemDidWakeUp: messages.SystemDidWakeUp = {
+	event: "systemDidWakeUp"
+};
+
+/**
+ * Mocked {@link messages.titleParametersDidChange} message.
+ */
+export const titleParametersDidChange: messages.TitleParametersDidChange<Settings> = {
+	action,
+	context,
+	device,
+	event: "titleParametersDidChange",
+	payload: {
+		coordinates: {
+			column: 3,
+			row: 4
+		},
+		settings: {
+			name: "Elgato"
+		},
+		title: "Hello world",
+		titleParameters: {
+			fontFamily: "Arial",
+			fontSize: 13,
+			fontStyle: "Bold Italic",
+			fontUnderline: true,
+			showTitle: false,
+			titleAlignment: "middle",
+			titleColor: "#ffffff"
+		},
+		state: 1
+	}
+};
+
+/**
+ * Mocked {@link messages.touchTap} message.
+ */
+export const touchTap: messages.TouchTap<Settings> = {
+	action,
+	context,
+	device,
+	event: "touchTap",
+	payload: {
+		coordinates: {
+			column: 4,
+			row: 1
+		},
+		hold: true,
+		settings: {
+			name: "Elgato"
+		},
+		tapPos: [10, 50]
+	}
+};
+
+/**
+ * Mocked {@link messages.willAppear} message.
+ */
+export const willAppear: messages.WillAppear<Settings> = {
+	action,
+	context,
+	device,
+	event: "willAppear",
+	payload: {
+		controller: "Keypad",
+		coordinates: {
+			column: 8,
+			row: 2
+		},
+		isInMultiAction: false,
+		settings: {
+			name: "Elgato"
+		},
+		state: 1
+	}
+};
+
+/**
+ * Mocked {@link messages.willDisappear} message.
+ */
+export const willDisappear: messages.WillDisappear<Settings> = {
+	...willAppear,
+	event: "willDisappear"
+};
+
+/**
+ * Mock settings.
+ */
+export type Settings = {
+	/**
+	 * Mock property.
+	 */
+	name: string;
 };
--- src/interactivity/client.test.ts
+++ src/interactivity/client.test.ts
@@ -1,8 +1,30 @@
 import { emitFrom } from "../../test/events";
-import * as messages from "../connectivity/messages";
+import * as mocks from "../connectivity/__mocks__/messages";
+import { InboundMessages } from "../connectivity/messages";
 import { Device } from "../devices";
+import { Action } from "./action";
 import { StreamDeckClient } from "./client";
-import { SendToPluginEvent } from "./events";
+import {
+	ApplicationDidLaunchEvent,
+	ApplicationDidTerminateEvent,
+	DeviceDidConnectEvent,
+	DeviceDidDisconnectEvent,
+	DialDownEvent,
+	DialRotateEvent,
+	DialUpEvent,
+	DidReceiveGlobalSettingsEvent,
+	DidReceiveSettingsEvent,
+	KeyDownEvent,
+	KeyUpEvent,
+	PropertyInspectorDidAppearEvent,
+	PropertyInspectorDidDisappearEvent,
+	SendToPluginEvent,
+	SystemDidWakeUpEvent,
+	TitleParametersDidChangeEvent,
+	TouchTapEvent,
+	WillAppearEvent,
+	WillDisappearEvent
+} from "./events";
 
 const { StreamDeckConnection } = jest.createMockFromModule<typeof import("../connectivity/connection")>("../connectivity/connection");
 
@@ -12,6 +34,466 @@ describe("StreamDeckClient", () => {
 	beforeEach(() => {
 		connection = new StreamDeckConnection();
 		jest.resetAllMocks();
+	});
+
+	/**
+	 * Emits the specified {@link message} from the connection.
+	 * @param message Message to emit from the connection.
+	 * @returns The {@link message}.
+	 */
+	function emitFromConnection<T extends InboundMessages>(message: T) {
+		emitFrom(connection, message.event, message);
+		return message;
+	}
+
+	/**
+	 * Asserts {@link StreamDeckClient.onApplicationDidLaunch} invokes the listener when the connection emits the `applicationDidLaunch` event.
+	 */
+	it("Receives onApplicationDidLaunch", () => {
+		// Arrange.
+		const client = new StreamDeckClient(connection, new Map<string, Device>());
+		const listener = jest.fn();
+		client.onApplicationDidLaunch(listener);
+
+		// Act.
+		const {
+			payload: { application }
+		} = emitFromConnection(mocks.applicationDidLaunch);
+
+		//Assert.
+		expect(listener).toHaveBeenCalledTimes(1);
+		expect(listener).toHaveBeenCalledWith<[ApplicationDidLaunchEvent]>({
+			application,
+			type: "applicationDidLaunch"
+		});
+	});
+
+	/**
+	 * Asserts {@link StreamDeckClient.onApplicationDidTerminate} invokes the listener when the connection emits the `applicationDidTerminate` event.
+	 */
+	it("Receives onApplicationDidTerminate", () => {
+		// Arrange.
+		const client = new StreamDeckClient(connection, new Map<string, Device>());
+		const listener = jest.fn();
+		client.onApplicationDidTerminate(listener);
+
+		// Act.
+		const {
+			payload: { application }
+		} = emitFromConnection(mocks.applicationDidTerminate);
+
+		//Assert.
+		expect(listener).toHaveBeenCalledTimes(1);
+		expect(listener).toHaveBeenCalledWith<[ApplicationDidTerminateEvent]>({
+			application,
+			type: "applicationDidTerminate"
+		});
+	});
+
+	/**
+	 * Asserts {@link StreamDeckClient.onDeviceDidConnect} invokes the listener when the connection emits the `deviceDidConnect` event.
+	 */
+	it("Receives onDeviceDidConnect", () => {
+		// Arrange.
+		const client = new StreamDeckClient(connection, new Map<string, Device>());
+		const listener = jest.fn();
+		client.onDeviceDidConnect(listener);
+
+		// Act.
+		const { device } = emitFromConnection(mocks.deviceDidConnect);
+
+		// Assert.
+		expect(listener).toHaveBeenCalledTimes(1);
+		expect(listener).toHaveBeenCalledWith<[DeviceDidConnectEvent]>({
+			device: {
+				id: device,
+				isConnected: true,
+				...mocks.deviceDidConnect.deviceInfo
+			},
+			type: mocks.deviceDidConnect.event
+		});
+	});
+
+	/**
+	 * Asserts {@link StreamDeckClient.onDeviceDidDisconnect} invokes the listener when the connection emits the `deviceDidDisconnect` event.
+	 */
+	it("Receives onDeviceDidDisconnect", () => {
+		// Arrange.
+		const client = new StreamDeckClient(connection, new Map<string, Device>());
+		const listener = jest.fn();
+		client.onDeviceDidDisconnect(listener);
+
+		// Act.
+		const { device } = emitFromConnection(mocks.deviceDidDisconnect);
+
+		// Assert.
+		expect(listener).toBeCalledTimes(1);
+		expect(listener).toHaveBeenCalledWith<[DeviceDidDisconnectEvent]>({
+			device: {
+				id: device,
+				isConnected: false,
+				name: undefined,
+				size: undefined,
+				type: undefined
+			},
+			type: "deviceDidDisconnect"
+		});
+	});
+
+	/**
+	 * Asserts {@link StreamDeckClient.onDeviceDidDisconnect} invokes the listener when the connection emits the `deviceDidDisconnect` event
+	 * reads from the device cache.
+	 */
+	it("Receives onDeviceDidDisconnect (Cache)", () => {
+		// Arrange.
+		const devices = new Map<string, Device>();
+		devices.set(mocks.deviceDidDisconnect.device, {
+			id: mocks.deviceDidConnect.device,
+			isConnected: true,
+			...mocks.deviceDidConnect.deviceInfo
+		});
+
+		const client = new StreamDeckClient(connection, devices);
+		const listener = jest.fn();
+		client.onDeviceDidDisconnect(listener);
+
+		// Act.
+		const { device } = emitFromConnection(mocks.deviceDidDisconnect);
+
+		// Assert.
+		expect(listener).toHaveBeenCalledTimes(1);
+		expect(listener).toHaveBeenCalledWith<[DeviceDidDisconnectEvent]>({
+			device: {
+				id: device,
+				isConnected: false,
+				...mocks.deviceDidConnect.deviceInfo
+			},
+			type: "deviceDidDisconnect"
+		});
+	});
+
+	/**
+	 * Asserts {@link StreamDeckClient.onDialDown} invokes the listener when the connection emits the `dialDown` event.
+	 */
+	it("Receives onDialDown", () => {
+		// Arrange.
+		const client = new StreamDeckClient(connection, new Map<string, Device>());
+		const listener = jest.fn();
+		client.onDialDown(listener);
+
+		// Act.
+		const { action, context, device, payload } = emitFromConnection(mocks.dialDown);
+
+		// Assert.
+		expect(listener).toHaveBeenCalledTimes(1);
+		expect(listener).toHaveBeenCalledWith<[DialDownEvent<mocks.Settings>]>({
+			action: new Action(client, action, context),
+			deviceId: device,
+			payload,
+			type: "dialDown"
+		});
+	});
+
+	/**
+	 * Asserts {@link StreamDeckClient.onDialRotate} invokes the listener when the connection emits the `dialRotate` event.
+	 */
+	it("Receives onDialRotate", () => {
+		// Arrange.
+		const client = new StreamDeckClient(connection, new Map<string, Device>());
+		const listener = jest.fn();
+		client.onDialRotate(listener);
+
+		// Act.
+		const { action, context, device, payload } = emitFromConnection(mocks.dialRotate);
+
+		// Assert.
+		expect(listener).toHaveBeenCalledTimes(1);
+		expect(listener).toHaveBeenCalledWith<[DialRotateEvent<mocks.Settings>]>({
+			action: new Action(client, action, context),
+			deviceId: device,
+			payload,
+			type: "dialRotate"
+		});
+	});
+
+	/**
+	 * Asserts {@link StreamDeckClient.onDialUp} invokes the listener when the connection emits the `dialUp` event.
+	 */
+	it("Receives onDialUp", () => {
+		// Arrange.
+		const client = new StreamDeckClient(connection, new Map<string, Device>());
+		const listener = jest.fn();
+		client.onDialUp(listener);
+
+		// Act.
+		const { action, context, device, payload } = emitFromConnection(mocks.dialUp);
+
+		// Assert.
+		expect(listener).toHaveBeenCalledTimes(1);
+		expect(listener).toHaveBeenCalledWith<[DialUpEvent<mocks.Settings>]>({
+			action: new Action(client, action, context),
+			deviceId: device,
+			payload,
+			type: "dialUp"
+		});
+	});
+
+	/**
+	 * Asserts {@link StreamDeckClient.onDidReceiveGlobalSettings} invokes the listener when the connection emits the `didReceiveGlobalSettings` event.
+	 */
+	it("Receives onDidReceiveGlobalSettings", () => {
+		// Arrange.
+		const client = new StreamDeckClient(connection, new Map<string, Device>());
+		const listener = jest.fn();
+		client.onDidReceiveGlobalSettings(listener);
+
+		// Act.
+		const {
+			payload: { settings }
+		} = emitFromConnection(mocks.didReceiveGlobalSettings);
+
+		// Assert.
+		expect(listener).toHaveBeenCalledTimes(1);
+		expect(listener).toHaveBeenCalledWith<[DidReceiveGlobalSettingsEvent<mocks.Settings>]>({
+			settings,
+			type: "didReceiveGlobalSettings"
+		});
+	});
+
+	/**
+	 * Asserts {@link StreamDeckClient.onDidReceiveSettings} invokes the listener when the connection emits the `didReceiveSettings` event.
+	 */
+	it("Receives onDidReceiveSettings", () => {
+		// Arrange.
+		const client = new StreamDeckClient(connection, new Map<string, Device>());
+		const listener = jest.fn();
+		client.onDidReceiveSettings(listener);
+
+		// Act.
+		const { action, context, device, payload } = emitFromConnection(mocks.didReceiveSettings);
+
+		// Assert.
+		expect(listener).toHaveBeenCalledTimes(1);
+		expect(listener).toHaveBeenCalledWith<[DidReceiveSettingsEvent<mocks.Settings>]>({
+			action: new Action(client, action, context),
+			deviceId: device,
+			payload,
+			type: "didReceiveSettings"
+		});
+	});
+
+	/**
+	 * Asserts {@link StreamDeckClient.onKeyDown} invokes the listener when the connection emits the `keyDown` event.
+	 */
+	it("Receives onKeyDown", () => {
+		// Arrange.
+		const client = new StreamDeckClient(connection, new Map<string, Device>());
+		const listener = jest.fn();
+		client.onKeyDown(listener);
+
+		// Act.
+		const { action, context, device, payload } = emitFromConnection(mocks.keyDown);
+
+		// Assert.
+		expect(listener).toHaveBeenCalledTimes(1);
+		expect(listener).toHaveBeenCalledWith<[KeyDownEvent<mocks.Settings>]>({
+			action: new Action(client, action, context),
+			deviceId: device,
+			payload,
+			type: "keyDown"
+		});
+	});
+
+	/**
+	 * Asserts {@link StreamDeckClient.onKeyUp} invokes the listener when the connection emits the `keyUp` event.
+	 */
+	it("Receives onKeyUp", () => {
+		// Arrange.
+		const client = new StreamDeckClient(connection, new Map<string, Device>());
+		const listener = jest.fn();
+		client.onKeyUp(listener);
+
+		// Act.
+		const { action, context, device, payload } = emitFromConnection(mocks.keyUp);
+
+		// Assert.
+		expect(listener).toHaveBeenCalledTimes(1);
+		expect(listener).toHaveBeenCalledWith<[KeyUpEvent<mocks.Settings>]>({
+			action: new Action(client, action, context),
+			deviceId: device,
+			payload,
+			type: "keyUp"
+		});
+	});
+
+	/**
+	 * Asserts {@link StreamDeckClient.onPropertyInspectorDidAppear} invokes the listener when the connection emits the `propertyInspectorDidAppear` event.
+	 */
+	it("Receives onPropertyInspectorDidAppear", () => {
+		// Arrange.
+		const client = new StreamDeckClient(connection, new Map<string, Device>());
+		const listener = jest.fn();
+		client.onPropertyInspectorDidAppear(listener);
+
+		// Act.
+		const { action, context, device } = emitFromConnection(mocks.propertyInspectorDidAppear);
+
+		// Assert.
+		expect(listener).toHaveBeenCalledTimes(1);
+		expect(listener).toHaveBeenCalledWith<[PropertyInspectorDidAppearEvent]>({
+			action: new Action(client, action, context),
+			deviceId: device,
+			type: "propertyInspectorDidAppear"
+		});
+	});
+
+	/**
+	 * Asserts {@link StreamDeckClient.onPropertyInspectorDidDisappear} invokes the listener when the connection emits the `propertyInspectorDidDisappear` event.
+	 */
+	it("Receives onPropertyInspectorDidDisappear", () => {
+		// Arrange.
+		const client = new StreamDeckClient(connection, new Map<string, Device>());
+		const listener = jest.fn();
+		client.onPropertyInspectorDidDisappear(listener);
+
+		// Act.
+		const { action, context, device } = emitFromConnection(mocks.propertyInspectorDidDisappear);
+
+		// Assert.
+		expect(listener).toHaveBeenCalledTimes(1);
+		expect(listener).toHaveBeenCalledWith<[PropertyInspectorDidDisappearEvent]>({
+			action: new Action(client, action, context),
+			deviceId: device,
+			type: "propertyInspectorDidDisappear"
+		});
+	});
+
+	/**
+	 * Asserts {@link StreamDeckClient.onSendToPlugin} invokes the listener when the connection emits the `sendToPlugin` event.
+	 */
+	it("Receives onSendToPlugin", () => {
+		// Arrange.
+		const client = new StreamDeckClient(connection, new Map<string, Device>());
+		const listener = jest.fn();
+		client.onSendToPlugin(listener);
+
+		// Act.
+		const { action, context, payload } = emitFromConnection(mocks.sendToPlugin);
+
+		// Assert.
+		expect(listener).toHaveBeenCalledTimes(1);
+		expect(listener).toHaveBeenCalledWith<[SendToPluginEvent<mocks.Settings>]>({
+			action: new Action(client, action, context),
+			payload,
+			type: "sendToPlugin"
+		});
+	});
+
+	/**
+	 * Asserts {@link StreamDeckClient.onSystemDidWakeUp} invokes the listener when the connection emits the `systemDidWakeUp` event.
+	 */
+	it("Receives onSystemDidWakeUp", () => {
+		// Arrange.
+		const client = new StreamDeckClient(connection, new Map<string, Device>());
+		const listener = jest.fn();
+		client.onSystemDidWakeUp(listener);
+
+		// Act.
+		emitFromConnection(mocks.systemDidWakeUp);
+
+		// Assert.
+		expect(listener).toHaveBeenCalledTimes(1);
+		expect(listener).toHaveBeenCalledWith<[SystemDidWakeUpEvent]>({
+			type: "systemDidWakeUp"
+		});
+	});
+
+	/**
+	 * Asserts {@link StreamDeckClient.onTitleParametersDidChange} invokes the listener when the connection emits the `titleParametersDidChange` event.
+	 */
+	it("Receives onTitleParametersDidChange", () => {
+		// Arrange.
+		const client = new StreamDeckClient(connection, new Map<string, Device>());
+		const listener = jest.fn();
+		client.onTitleParametersDidChange(listener);
+
+		// Act.
+		const { action, context, device, payload } = emitFromConnection(mocks.titleParametersDidChange);
+
+		// Assert.
+		expect(listener).toHaveBeenCalledTimes(1);
+		expect(listener).toHaveBeenCalledWith<[TitleParametersDidChangeEvent<mocks.Settings>]>({
+			action: new Action(client, action, context),
+			deviceId: device,
+			payload,
+			type: "titleParametersDidChange"
+		});
+	});
+
+	/**
+	 * Asserts {@link StreamDeckClient.onTouchTap} invokes the listener when the connection emits the `touchTap` event.
+	 */
+	it("Receives onTouchTap", () => {
+		// Arrange.
+		const client = new StreamDeckClient(connection, new Map<string, Device>());
+		const listener = jest.fn();
+		client.onTouchTap(listener);
+
+		// Act.
+		const { action, context, device, payload } = emitFromConnection(mocks.touchTap);
+
+		// Assert.
+		expect(listener).toHaveBeenCalledTimes(1);
+		expect(listener).toHaveBeenCalledWith<[TouchTapEvent<mocks.Settings>]>({
+			action: new Action(client, action, context),
+			deviceId: device,
+			payload,
+			type: "touchTap"
+		});
+	});
+
+	/**
+	 * Asserts {@link StreamDeckClient.onWillAppear} invokes the listener when the connection emits the `willAppear` event.
+	 */
+	it("Receives onWillAppear", () => {
+		// Arrange.
+		const client = new StreamDeckClient(connection, new Map<string, Device>());
+		const listener = jest.fn();
+		client.onWillAppear(listener);
+
+		// Act.
+		const { action, context, device, payload } = emitFromConnection(mocks.willAppear);
+
+		// Assert.
+		expect(listener).toHaveBeenCalledTimes(1);
+		expect(listener).toHaveBeenCalledWith<[WillAppearEvent<mocks.Settings>]>({
+			action: new Action(client, action, context),
+			deviceId: device,
+			payload,
+			type: "willAppear"
+		});
+	});
+
+	/**
+	 * Asserts {@link StreamDeckClient.onWillDisappear} invokes the listener when the connection emits the `willDisappear` event.
+	 */
+	it("Receives onWillAppear", () => {
+		// Arrange.
+		const client = new StreamDeckClient(connection, new Map<string, Device>());
+		const listener = jest.fn();
+		client.onWillDisappear(listener);
+
+		// Act.
+		const { action, context, device, payload } = emitFromConnection(mocks.willDisappear);
+
+		// Assert.
+		expect(listener).toHaveBeenCalledTimes(1);
+		expect(listener).toHaveBeenCalledWith<[WillDisappearEvent<mocks.Settings>]>({
+			action: new Action(client, action, context),
+			deviceId: device,
+			payload,
+			type: "willDisappear"
+		});
 	});
 
 	/**
@@ -35,50 +517,4 @@ describe("StreamDeckClient", () => {
 			}
 		});
 	});
-
-	/**
-	 * Asserts {@link StreamDeckClient.sendToPlugin} is received from the {@link StreamDeckConnection}.
-	 */
-	it("Receives onSendToPlugin", () => {
-		// Arrange.
-		const client = new StreamDeckClient(connection, new Map<string, Device>());
-		let verified = false;
-
-		// Act.
-		client.onSendToPlugin(verify);
-		emitFrom<"sendToPlugin", messages.SendToPlugin>(connection, "sendToPlugin", {
-			action: "com.elgato.tests.one",
-			context: "abc123",
-			event: "sendToPlugin",
-			payload: {
-				name: "Elgato"
-			}
-		});
-
-		// Assert
-		expect(verified).toBe(true);
-
-		/**
-		 * Verifies {@link StreamDeckClient.onSendToPlugin}.
-		 * @param ev Event arguments.
-		 */
-		function verify(ev: SendToPluginEvent<Settings>) {
-			expect(ev.action.id).toBe("abc123");
-			expect(ev.action.manifestId).toBe("com.elgato.tests.one");
-			expect(ev.payload.name).toBe("Elgato");
-			expect(ev.type).toBe("sendToPlugin");
-
-			verified = true;
-		}
-	});
 });
-
-/**
- * Mock settings.
- */
-type Settings = {
-	/**
-	 * Mock property.
-	 */
-	name: string;
-};
