f3fe94e4101630c5f420939b02645af866f0f0c9
Richard Herman <geekyeggo@users.noreply.github.com>
2023-07-17 17:21:01 +0100

    Update comments and fix references

--- src/interactivity/action.ts
+++ src/interactivity/action.ts
@@ -1,22 +1,24 @@
 import { State } from "../connectivity/messages";
+import type { Manifest } from "../manifest";
+import type { SingletonAction } from "../routing/singleton-action";
 import type { StreamDeckClient } from "./client";
 import { FeedbackPayload } from "./layouts";
 import { Target } from "./target";
 
 /**
- * Provides a contextualized instance of `Action`, allowing for direct communication with an instance of an action.
+ * Provides a contextualized instance of an {@link Action}, allowing for direct communication with the Stream Deck.
  */
 export class Action {
 	/**
 	 * Initializes a new instance of the {@see Action} class.
 	 * @param client The Stream Deck client.
-	 * @param manifestId Unique identifier of the action as defined within the plugin's manifest (`Actions[].UUID`).
+	 * @param manifestId Unique identifier (UUID) of the action as defined within the plugin's manifest {@link Manifest.Actions} collection.
 	 * @param id Unique identifier of the instance of the action; this can be used to update the action on the Stream Deck, e.g. its title, settings, etc.
 	 */
 	constructor(private readonly client: StreamDeckClient, public readonly manifestId: string, public readonly id: string) {}
 
 	/**
-	 * Gets the settings associated with an instance of an action, as identified by the `context`. An instance of an action represents a button, dial, pedal, etc. Use in conjunction
+	 * Gets the settings associated this action instance.
 	 * with {@link Action.setSettings}.
 	 * @returns Promise containing the action instance's settings.
 	 */
@@ -25,21 +27,20 @@ export class Action {
 	}
 
 	/**
-	 * Sends the `payload` to the current property inspector associated with an instance of an action, as identified by the `context`. The plugin can also receive information from
-	 * the property inspector via the `"sendToPlugin"` event, allowing for bi-directional communication. **Note**, the `payload` is only received by the property inspector when it
-	 * is associated with the specified `context`.
+	 * Sends the {@link payload} to the current property inspector associated with this action instance. The plugin can also receive information from the property inspector via {@link StreamDeckClient.onSendToPlugin}
+	 * and {@link SingletonAction.onSendToPlugin} allowing for bi-directional communication.
 	 * @param payload Payload to send to the property inspector.
-	 * @returns `Promise` resolved when the request to send the `payload` to the property inspector has been sent to Stream Deck.
+	 * @returns `Promise` resolved when {@link payload} has been sent to the property inspector.
 	 */
 	public sendToPropertyInspector(payload: unknown): Promise<void> {
 		return this.client.sendToPropertyInspector(this.id, payload);
 	}
 
 	/**
-	 * Sets the feedback of a layout associated with an action instance allowing for visual items to be updated. Layouts are a powerful way to provide dynamic information to users,
-	 * and can be assigned in the manifest, or dynamically via {@link Action.setFeedbackLayout}.
+	 * Sets the feedback for the current layout associated with this action instance, allowing for the visual items to be updated. Layouts are a powerful way to provide dynamic information
+	 * to users, and can be assigned in the {@link Manifest.Actions} manifest, or dynamically via {@link Action.setFeedbackLayout}.
 	 *
-	 * The `feedback` payload defines which items within the layout should be updated, and are identified by their property name (defined as the `key` in the layout's definition).
+	 * The {@link feedback} payload defines which items within the layout will be updated, and are identified by their property name (defined as the `key` in the layout's definition).
 	 * The values can either by a complete new definition, a `string` for layout item types of `text` and `pixmap`, or a `number` for layout item types of `bar` and `gbar`.
 	 *
 	 * For example, given the following custom layout definition saved relatively to the plugin at "layouts/MyCustomLayout.json".
@@ -85,16 +86,15 @@ export class Action {
 	 * });
 	 * ```
 	 * @param feedback Object containing information about the layout items to be updated.
-	 * @returns `Promise` resolved when the request to set the `feedback` has been sent to Stream Deck.
+	 * @returns `Promise` resolved when the request to set the {@link feedback} has been sent to Stream Deck.
 	 */
 	public setFeedback(feedback: FeedbackPayload): Promise<void> {
 		return this.client.setFeedback(this.id, feedback);
 	}
 
 	/**
-	 * Sets the layout associated with an action instance, as identified by the `context`. The layout must be either a built-in layout identifier, or path to a local layout JSON file
-	 * within the plugin's folder.
-	 * Use in conjunction with {@link Action.setFeedback} to update the layouts current settings once it has been changed.
+	 * Sets the layout associated with this action instance. The layout must be either a built-in layout identifier, or path to a local layout JSON file within the plugin's folder.
+	 * Use in conjunction with {@link Action.setFeedback} to update the layout's current items' settings.
 	 * @param layout Name of a pre-defined layout, or relative path to a custom one.
 	 * @returns `Promise` resolved when the new layout has been sent to Stream Deck.
 	 */
@@ -103,29 +103,28 @@ export class Action {
 	}
 
 	/**
-	 * Sets the `image` displayed for an instance of an action, as identified by the `context`.
+	 * Sets the {@link image} to be display for this action instance. **NB** This image will be ignored if the user has chosen a custom image within the Stream Deck application.
 	 * @param image Image to display; this can be either a path to a local file within the plugin's folder, a base64 encoded `string` with the mime type declared (e.g. PNG, JPEG, etc.),
-	 * or an SVG `string`. When `image` is `undefined`, the image from the manifest is used.
-	 * @param state Action state the request applies to; when no state is supplied, the image is set for both states. **Note**, only applies to multi-state actions.
+	 * or an SVG `string`. When {@link image} is `undefined`, the image from the manifest is used.
+	 * @param state Action state the request applies to; when no state is supplied, the image is set for both states. **NB** Only applies to multi-state actions.
 	 * @param target Specifies which aspects of the Stream Deck should be updated, hardware, software, or both.
-	 * @returns `Promise` resolved when the request to set the `image` has been sent to Stream Deck.
+	 * @returns `Promise` resolved when the request to set the {@link image} has been sent to Stream Deck.
 	 */
 	public setImage(image: string, state: State | undefined = undefined, target: Target = Target.HardwareAndSoftware): Promise<void> {
 		return this.client.setImage(this.id, image, state, target);
 	}
 
 	/**
-	 * Sets the `settings` associated with an instance of an action, as identified by the `context`. An instance of an action represents a button, dial, pedal, etc.
-	 * Use in conjunction with {@link Action.getSettings}.
-	 * @param settings Settings to associate with the action instance.
-	 * @returns `Promise` resolved when the `settings` are sent to Stream Deck.
+	 * Sets the {@link settings} associated with this action instance. Use in conjunction with {@link Action.getSettings}.
+	 * @param settings Settings to persist.
+	 * @returns `Promise` resolved when the {@link settings} are sent to Stream Deck.
 	 */
 	public setSettings(settings: unknown): Promise<void> {
 		return this.client.setSettings(this.id, settings);
 	}
 
 	/**
-	 * Sets the current state of an action instance; this only applies to actions that have multiple states defined within the manifest.json file.
+	 * Sets the current {@link state} of this action instance; only applies to actions that have multiple states defined within the manifest JSON file.
 	 * @param state State to set; this be either 0, or 1.
 	 * @returns `Promise` resolved when the request to set the state of an action instance has been sent to Stream Deck.
 	 */
@@ -134,19 +133,19 @@ export class Action {
 	}
 
 	/**
-	 * Sets the `title` displayed for an instance of an action, as identified by the `context`. Often used in conjunction with `"titleParametersDidChange"` event.
+	 * Sets the {@link title} displayed for this action instance. Often used in conjunction with the {@link StreamDeckClient.onTitleParametersDidChange} / {@link SingletonAction.onTitleParametersDidChange}
+	 * event.
 	 * @param title Title to display; when no title is specified, the title will reset to the title set by the user.
 	 * @param state Action state the request applies to; when no state is supplied, the title is set for both states. **Note**, only applies to multi-state actions.
 	 * @param target Specifies which aspects of the Stream Deck should be updated, hardware, software, or both.
-	 * @returns `Promise` resolved when the request to set the `title` has been sent to Stream Deck.
+	 * @returns `Promise` resolved when the request to set the {@link title} has been sent to Stream Deck.
 	 */
 	public setTitle(title?: string, state: State | undefined = undefined, target: Target = Target.HardwareAndSoftware): Promise<void> {
 		return this.client.setTitle(this.id, title, state, target);
 	}
 
 	/**
-	 * Temporarily shows an alert (i.e. warning), in the form of an exclamation mark in a yellow triangle, on an action, as identified by the `context`. Used to provide visual feedback
-	 * when an action failed.
+	 * Temporarily shows an alert (i.e. warning), in the form of an exclamation mark in a yellow triangle, on this action instance. Used to provide visual feedback when an action failed.
 	 * @returns `Promise` resolved when the request to show an alert has been sent to Stream Deck.
 	 */
 	public showAlert(): Promise<void> {
@@ -154,8 +153,8 @@ export class Action {
 	}
 
 	/**
-	 * Temporarily shows an "OK" (i.e. success), in the form of a check-mark in a green circle, on an action, as identified by the `context`. Used to provide visual feedback when an
-	 * action successfully executed.
+	 * Temporarily shows an "OK" (i.e. success), in the form of a check-mark in a green circle, on this action instance. Used to provide visual feedback when an action successfully
+	 * executed.
 	 * @returns `Promise` resolved when the request to show an "OK" has been sent to Stream Deck.
 	 */
 	public showOk(): Promise<void> {
