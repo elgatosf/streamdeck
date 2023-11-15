650d798f2f76e1446af68157671b569d635dd442
Richard Herman <geekyeggo@users.noreply.github.com>
2023-11-15 14:12:20 +0000

    fix: typing of event emitter

--- src/common/typed-event-emitter.ts
+++ src/common/typed-event-emitter.ts
@@ -7,13 +7,14 @@ import type { EventEmitter } from "node:events";
  * Map of event listeners, indexed by their event name.
  */
 export type EventMap = {
-	[key: string]: (...args: unknown[]) => void;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	[key: string]: (...args: any[]) => void;
 };
 
 /**
  * An {@link EventEmitter} with typed event names and listeners.
  */
-export interface TypedEventEmitter<M> {
+export interface TypedEventEmitter<M extends EventMap> {
 	/** @inheritdoc */
 	addListener<E extends keyof M>(event: E, listener: M[E]): this;
 
@@ -39,7 +40,7 @@ export interface TypedEventEmitter<M> {
 	removeListener<E extends keyof M>(event: E, listener: M[E]): this;
 
 	/** @inheritdoc */
-	emit<E extends keyof M>(event: E, data: M[E]): boolean;
+	emit<E extends keyof M>(event: E, ...args: Parameters<M[E]>): boolean;
 
 	/** @inheritdoc */
 	rawListeners<E extends keyof M>(event: E): M[E][];
