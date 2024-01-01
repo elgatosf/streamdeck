1474b06de330245484e612a647a6e9fb48069320
Richard Herman <geekyeggo@users.noreply.github.com>
2024-01-01 22:00:14 +0000

    feat: export polyfill for re-use

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { StreamDeck } from "./stream-deck";
 export { Action } from "./actions/action";
 export { action } from "./actions/decorators";
 export { SingletonAction } from "./actions/singleton-action";
+export { EventEmitter, EventsOf } from "./common/event-emitter";
 export * from "./connectivity/layouts";
 export { Target } from "./connectivity/target";
 export * from "./events";
