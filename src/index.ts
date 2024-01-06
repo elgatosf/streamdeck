666300ab68906e05d174d1ddb619511a93357f82
Richard Herman <geekyeggo@users.noreply.github.com>
2024-01-06 19:19:14 +0000

    fix: export constraints to enable extending SingletonAction

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { StreamDeck } from "./stream-deck";
 export { Action } from "./actions/action";
 export { action } from "./actions/decorators";
 export { SingletonAction } from "./actions/singleton-action";
+export { PayloadObject } from "./connectivity/events";
 export * from "./connectivity/layouts";
 export { Target } from "./connectivity/target";
 export * from "./events";
