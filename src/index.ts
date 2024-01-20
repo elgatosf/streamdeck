a70641d37ef545efa4d74bcc140e0ea0af072161
Richard Herman <geekyeggo@users.noreply.github.com>
2024-01-20 17:52:36 +0000

    fix: export DeviceType enum

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { StreamDeck } from "./stream-deck";
 export { Action } from "./actions/action";
 export { action } from "./actions/decorators";
 export { SingletonAction } from "./actions/singleton-action";
+export { DeviceType } from "./api/device";
 export { Target } from "./api/target";
 export { EventEmitter, EventsOf } from "./common/event-emitter";
 export * from "./connectivity/layouts";
