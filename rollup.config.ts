b91556fa05f6124de012940099cd4aad727c1046
Richard Herman <geekyeggo@users.noreply.github.com>
2024-03-22 13:42:49 +0000

    build: remove rollup bundling of declarations to prevent loss of summaries
    Sadly, when utilizing rollup-plugin-dts to bundle declarations into a single file, summaries for "export import *" were being lost. This result in quiet issues when creating the .d.ts file, so has [temporarily] been removed.

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,8 +2,7 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import { dirname, join, parse, resolve } from "node:path";
 import url from "node:url";
-import { NormalizedOutputOptions, OutputBundle, RollupOptions } from "rollup";
-import dts from "rollup-plugin-dts";
+import { RollupOptions } from "rollup";
 
 const isWatching = !!process.env.ROLLUP_WATCH;
 const banner = `/**!
@@ -43,33 +42,6 @@ function getOptions(input: string, output: string): RollupOptions[] {
 					mapRoot: isWatching ? "./" : undefined
 				}),
 				nodeResolve()
-			]
-		},
-		/**
-		 * TypeScript declarations.
-		 */
-		{
-			input,
-			output: {
-				file: `${outputFileWithoutExtension}.d.ts`
-			},
-			external: ["@elgato/schemas/streamdeck/plugins"],
-			plugins: [
-				dts(),
-				{
-					name: "NameChecker",
-					generateBundle: function (options: NormalizedOutputOptions, bundle: OutputBundle): void {
-						// Search each file for variable names that resemble possible duplicates, e.g. ActionEvent$1, Event$1.
-						const warnings = new Set(
-							Object.values(bundle).reduce<string[]>((names, file) => ("code" in file ? [...names, ...(file.code.match(/[A-Za-z0-9\-_]+?\$\d/gm) || [])] : names), [])
-						);
-
-						// And warn for each possible duplicate.
-						if (warnings.size) {
-							warnings.forEach((value) => this.warn(`Type was renamed "${value}"`));
-						}
-					}
-				}
 			]
 		}
 	];
