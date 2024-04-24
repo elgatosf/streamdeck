a74f96b0c798c651182f7fed347659aa23a8c649
Richard Herman <geekyeggo@users.noreply.github.com>
2024-04-24 14:22:24 +0100

    docs: fix param reference and local aliases

--- src/plugin/actions/singleton-action.ts
+++ src/plugin/actions/singleton-action.ts
@@ -28,91 +28,91 @@ export class SingletonAction<T extends JsonObject = JsonObject> {
 	public readonly manifestId: string | undefined;
 
 	/**
-	 * Occurs when the user presses a dial (Stream Deck +). Also see {@link streamDeck.actions.onDialUp}.
+	 * Occurs when the user presses a dial (Stream Deck +). Also see {@link SingletonAction.onDialUp}.
 	 *
-	 * NB: For other action types see {@link streamDeck.actions.onKeyDown}.
-	 * @param listener Function to be invoked when the event occurs.
+	 * NB: For other action types see {@link SingletonAction.onKeyDown}.
+	 * @param ev The event.
 	 */
 	public onDialDown?(ev: DialDownEvent<T>): Promise<void> | void;
 
 	/**
 	 * Occurs when the user rotates a dial (Stream Deck +).
-	 * @param listener Function to be invoked when the event occurs.
+	 * @param ev The event.
 	 */
 	public onDialRotate?(ev: DialRotateEvent<T>): Promise<void> | void;
 
 	/**
-	 * Occurs when the user releases a pressed dial (Stream Deck +). Also see {@link streamDeck.actions.onDialDown}.
+	 * Occurs when the user releases a pressed dial (Stream Deck +). Also see {@link SingletonAction.onDialDown}.
 	 *
-	 * NB: For other action types see {@link streamDeck.actions.onKeyUp}.
-	 * @param listener Function to be invoked when the event occurs.
+	 * NB: For other action types see {@link SingletonAction.onKeyUp}.
+	 * @param ev The event.
 	 */
 	public onDialUp?(ev: DialUpEvent<T>): Promise<void> | void;
 
 	/**
 	 * Occurs when the settings associated with an action instance are requested using {@link Action.getSettings}, or when the the settings were updated by the property inspector.
-	 * @param listener Function to be invoked when the event occurs.
+	 * @param ev The event.
 	 */
 	public onDidReceiveSettings?(ev: DidReceiveSettingsEvent<T>): Promise<void> | void;
 
 	/**
-	 * Occurs when the user presses a action down. Also see {@link streamDeck.actions.onKeyUp}.
+	 * Occurs when the user presses a action down. Also see {@link SingletonAction.onKeyUp}.
 	 *
-	 * NB: For dials / touchscreens see {@link streamDeck.actions.onDialDown}.
-	 * @param listener Function to be invoked when the event occurs.
+	 * NB: For dials / touchscreens see {@link SingletonAction.onDialDown}.
+	 * @param ev The event.
 	 */
 	public onKeyDown?(ev: KeyDownEvent<T>): Promise<void> | void;
 
 	/**
-	 * Occurs when the user releases a pressed action. Also see {@link streamDeck.actions.onKeyDown}.
+	 * Occurs when the user releases a pressed action. Also see {@link SingletonAction.onKeyDown}.
 	 *
-	 * NB: For dials / touchscreens see {@link streamDeck.actions.onDialUp}.
-	 * @param listener Function to be invoked when the event occurs.
+	 * NB: For dials / touchscreens see {@link SingletonAction.onDialUp}.
+	 * @param ev The event.
 	 */
 	public onKeyUp?(ev: KeyUpEvent<T>): Promise<void> | void;
 
 	/**
-	 * Occurs when the property inspector associated with the action becomes visible, i.e. the user selected an action in the Stream Deck application. Also see {@link streamDeck.ui.onPropertyInspectorDidDisappear}.
-	 * @param listener Function to be invoked when the event occurs.
+	 * Occurs when the property inspector associated with the action becomes visible, i.e. the user selected an action in the Stream Deck application. Also see {@link streamDeck.ui.onDidAppear}.
+	 * @param ev The event.
 	 */
 	public onPropertyInspectorDidAppear?(ev: PropertyInspectorDidAppearEvent<T>): Promise<void> | void;
 
 	/**
-	 * Occurs when the property inspector associated with the action becomes invisible, i.e. the user unselected the action in the Stream Deck application. Also see {@link streamDeck.ui.onPropertyInspectorDidAppear}.
-	 * @param listener Function to be invoked when the event occurs.
+	 * Occurs when the property inspector associated with the action becomes invisible, i.e. the user unselected the action in the Stream Deck application. Also see {@link streamDeck.ui.onDidDisappear}.
+	 * @param ev The event.
 	 */
 	public onPropertyInspectorDidDisappear?(ev: PropertyInspectorDidDisappearEvent<T>): Promise<void> | void;
 
 	/**
 	 * Occurs when a message was sent to the plugin _from_ the property inspector. The plugin can also send messages _to_ the property inspector using {@link Action.sendToPropertyInspector}.
 	 * @deprecated Consider using {@link streamDeck.ui.registerRoute} to receive requests from the property inspector.
-	 * @param listener Function to be invoked when the event occurs.
+	 * @param ev The event.
 	 */
 	public onSendToPlugin?(ev: SendToPluginEvent<JsonValue, T>): Promise<void> | void;
 
 	/**
 	 * Occurs when the user updates an action's title settings in the Stream Deck application. Also see {@link Action.setTitle}.
-	 * @param listener Function to be invoked when the event occurs.
+	 * @param ev The event.
 	 */
 	public onTitleParametersDidChange?(ev: TitleParametersDidChangeEvent<T>): Promise<void> | void;
 
 	/**
 	 * Occurs when the user taps the touchscreen (Stream Deck +).
-	 * @param listener Function to be invoked when the event occurs.
+	 * @param ev The event.
 	 */
 	public onTouchTap?(ev: TouchTapEvent<T>): Promise<void> | void;
 
 	/**
 	 * Occurs when an action appears on the Stream Deck due to the user navigating to another page, profile, folder, etc. This also occurs during startup if the action is on the "front
 	 * page". An action refers to _all_ types of actions, e.g. keys, dials,
-	 * @param listener Function to be invoked when the event occurs.
+	 * @param ev The event.
 	 */
 	public onWillAppear?(ev: WillAppearEvent<T>): Promise<void> | void;
 
 	/**
 	 * Occurs when an action disappears from the Stream Deck due to the user navigating to another page, profile, folder, etc. An action refers to _all_ types of actions, e.g. keys,
 	 * dials, touchscreens, pedals, etc.
-	 * @param listener Function to be invoked when the event occurs.
+	 * @param ev The event.
 	 */
 	public onWillDisappear?(ev: WillDisappearEvent<T>): Promise<void> | void;
 }
