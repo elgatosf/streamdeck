c7a5663df76b0266afc5774f873d0fad9449a1e2
Richard Herman <geekyeggo@users.noreply.github.com>
2024-10-09 15:33:01 +0100

    chore: standardize build for UI and plugin

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,95 +1,61 @@
 import nodeResolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
-import { dirname, resolve } from "node:path";
+import { dirname, join, parse, resolve } from "node:path";
 import url from "node:url";
 
 const isWatching = !!process.env.ROLLUP_WATCH;
-const external = ["ws", "@elgato/schemas/streamdeck/plugins"];
-
-const output = {
-	banner: `/**!
+const banner = `/**!
  * @author Elgato
  * @module elgato/streamdeck
  * @license MIT
  * @copyright Copyright (c) Corsair Memory Inc.
- */`,
-	sourcemap: isWatching,
-	sourcemapPathTransform: (relativeSourcePath, sourcemapPath) => {
-		return url.pathToFileURL(resolve(dirname(sourcemapPath), relativeSourcePath)).href;
-	}
-};
+ */`;
 
 /**
- * Generates a wrapped DTS file.
- * @param {string} index File path to the index.d.ts file.
- * @returns The wrapped DTS file.
+ * Defines a rollup configuration for the specified input and output.
+ * @param {string} input Path to the input TypeScript file, with "src/" omitted.
+ * @param {string} output Path to the desired output JavaScript file, with "dist/" omitted.
+ * @returns Rollup configuration.
  */
-function dts(index) {
-	return `import streamDeck from "${index}";
+function defineConfig(input, output) {
+	return {
+		input: join("src", input),
+		output: {
+			banner,
+			file: join("dist", output),
+			sourcemap: isWatching,
+			sourcemapPathTransform: (relativeSourcePath, sourcemapPath) => {
+				return url.pathToFileURL(resolve(dirname(sourcemapPath), relativeSourcePath)).href;
+			}
+		},
+		external: ["ws", "@elgato/schemas/streamdeck/plugins"],
+		plugins: [
+			typescript({
+				tsconfig: join("src", dirname(input), "tsconfig.build.json"),
+				mapRoot: isWatching ? "./" : undefined
+			}),
+			nodeResolve(),
+			{
+				name: "emit-dts",
+				generateBundle() {
+					const types = `"../types/${dirname(input)}"`;
+					this.emitFile({
+						fileName: `${parse(output).name}.d.ts`,
+						type: "asset",
+						source: `${banner}
+import streamDeck from ${types};
 
-export * from "${index}";
-export default streamDeck;`;
+export * from ${types};
+export default streamDeck;
+`
+					});
+				}
+			}
+		]
+	};
 }
 
 /**
  * Rollup configuration.
  */
-export default [
-	/**
-	 * Main build.
-	 */
-	{
-		input: "src/plugin/index.ts",
-		output: {
-			...output,
-			file: `dist/index.js`
-		},
-		external,
-		plugins: [
-			typescript({
-				tsconfig: "src/plugin/tsconfig.build.json",
-				mapRoot: isWatching ? "./" : undefined
-			}),
-			nodeResolve(),
-			{
-				name: "emit-dts",
-				generateBundle() {
-					this.emitFile({
-						fileName: "index.d.ts",
-						source: dts("../types/plugin"),
-						type: "asset"
-					});
-				}
-			}
-		]
-	},
-
-	/**
-	 * Browser build.
-	 */
-	{
-		input: "src/ui/index.ts",
-		output: {
-			...output,
-			file: `dist/browser.js`
-		},
-		external,
-		plugins: [
-			typescript({
-				tsconfig: "src/ui/tsconfig.build.json",
-				mapRoot: isWatching ? "./" : undefined
-			}),
-			nodeResolve(),
-			{
-				name: "emit-dts",
-				generateBundle() {
-					this.emitFile({
-						fileName: "browser.d.ts",
-						source: dts("../types/ui"),
-						type: "asset"
-					});
-				}
-			}
-		]
-	}
-];
+export default [defineConfig("plugin/index.ts", "index.js"), defineConfig("ui/index.ts", "browser.js")];
