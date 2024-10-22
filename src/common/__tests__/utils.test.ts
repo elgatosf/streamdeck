b96833bda99cae5f5b6c639df801045c76eba125
Richard Herman <geekyeggo@users.noreply.github.com>
2024-10-22 19:43:27 +0100

    test: set() utility function

--- src/common/__tests__/utils.test.ts
+++ src/common/__tests__/utils.test.ts
@@ -1,4 +1,4 @@
-import { freeze, get } from "../utils";
+import { freeze, get, set } from "../utils";
 
 /**
  * Provides assertions for {@link freeze}.
@@ -57,23 +57,68 @@ describe("freeze", () => {
  * Provides assertions for {@link get}.
  */
 describe("get", () => {
-	it("gets the value for a top-level path", () => {
+	it("should retrieve value of simple path", () => {
+		// Arrange, act, assert.
 		const obj = { foo: "bar" };
 		expect(get("foo", obj)).toBe("bar");
 	});
 
-	it("gets the value for a nested path", () => {
+	it("should retrieve value of nested path", () => {
+		// Arrange, act, assert.
 		const obj = { nested: { number: 13 } };
 		expect(get("nested.number", obj)).toBe(13);
 	});
 
-	it("handles falsy values", () => {
+	it("should retrieve value of path that returns falsy", () => {
+		// Arrange, act, assert.
 		const obj = { falsy: false };
 		expect(get("falsy", obj)).toBe(false);
 	});
 
-	it("defaults to undefined", () => {
+	it("should retrieve undefined when the property does not exist", () => {
+		// Arrange, act, assert.
 		const obj = {};
 		expect(get("__unknown.__prop", obj)).toBe(undefined);
+	});
+});
+
+/**
+ * Provides assertions for {@link set}.
+ */
+describe("set", () => {
+	it("should set value of simple path", () => {
+		// Arrange, act.
+		const obj = { foo: "Hello" };
+		set("foo", obj, "Good bye");
+
+		// Assert.
+		expect(obj.foo).toBe("Good bye");
+	});
+
+	it("should set value of nested path", () => {
+		// Arrange, act.
+		const obj = { nested: { number: 13 } };
+		set("nested.number", obj, 101);
+
+		// Assert.
+		expect(obj.nested.number).toBe(101);
+	});
+
+	it("should add value of simple path", () => {
+		// Arrange, act.
+		const obj: Record<string, unknown> = {};
+		set("foo", obj, "bar");
+
+		// Assert.
+		expect(obj.foo).toBe("bar");
+	});
+
+	it("should add value of nested path", () => {
+		// Arrange, act.
+		const obj: Record<string, Record<string, unknown>> = { nested: {} };
+		set("nested.number", obj, 13);
+
+		// Assert.
+		expect(obj.nested.number).toBe(13);
 	});
 });
