722fa6c6b9c224aa1edd8214f2c2c45cdb579bbb
Richard Herman <geekyeggo@users.noreply.github.com>
2024-10-22 19:46:37 +0100

    test: debounce utility

--- src/common/__tests__/utils.test.ts
+++ src/common/__tests__/utils.test.ts
@@ -1,4 +1,69 @@
-import { freeze, get, set } from "../utils";
+import { PromiseCompletionSource } from "../promises";
+import { debounce, freeze, get, set } from "../utils";
+
+/**
+ * Provides assertions for {@link debounce}.
+ */
+describe("debounce", () => {
+	it("should only invoke the function once before completing", async () => {
+		// Arrange.
+		let callCount = 0;
+		const fn = debounce(() => {
+			callCount++;
+		}, 1);
+
+		// when, then.
+		await Promise.all([fn(), fn(), fn()]);
+		expect(callCount).toBe(1);
+	});
+
+	it("should wait for the delay before invoking the callback", async () => {
+		// Arrange.
+		const awaiter = new PromiseCompletionSource<void>();
+		const fn = debounce(() => awaiter.setResult(), 100);
+
+		// Act.
+		const start = Math.floor(performance.now());
+
+		await fn();
+		await awaiter.promise;
+
+		const elapsed = Math.ceil(performance.now()) - start;
+
+		// Assert.
+		expect(elapsed).toBeGreaterThanOrEqual(100);
+	});
+
+	it("should reset the promise after each callback", async () => {
+		// Arrange.
+		let callCount = 0;
+		const fn = debounce(() => {
+			callCount++;
+		}, 1);
+
+		// when, then.
+		await fn();
+		expect(callCount).toBe(1);
+
+		// when, then.
+		await fn();
+		expect(callCount).toBe(2);
+	});
+
+	it("should invoke callback before setting promise completion source", async () => {
+		// Arrange.
+		let callbackTime!: number;
+
+		// Act.
+		const fn = debounce(() => {
+			callbackTime = performance.now();
+		}, 100);
+		await fn();
+
+		// Assert.
+		expect(performance.now()).toBeGreaterThan(callbackTime);
+	});
+});
 
 /**
  * Provides assertions for {@link freeze}.
