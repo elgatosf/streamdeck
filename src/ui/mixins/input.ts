34106dcc3aa5e7400fdc361f2d7280ccbc769d24
Richard Herman <geekyeggo@users.noreply.github.com>
2025-02-04 17:25:33 +0000

    fix: temporarily reintroduce brand on input mixin

--- src/ui/mixins/input.ts
+++ src/ui/mixins/input.ts
@@ -4,6 +4,8 @@ import { createRef, type Ref } from "lit/directives/ref.js";
 
 import type { Constructor } from "../../common/utils";
 
+const brand = "SDInputElement" as const;
+
 /**
  * Mixin that provides common functionality for input elements.
  * @param superClass Class the mixin extends.
@@ -16,6 +18,11 @@ export const Input = <TBase extends Constructor<LitElement> = typeof LitElement>
 	 * Mixin that provides common functionality for input elements.
 	 */
 	class InputMixin extends superClass {
+		/**
+		 * Brands the mixin, allowing us to export a helper method for type-narrowing.
+		 */
+		public __brand = brand;
+
 		/**
 		 * @inheritdoc
 		 */
@@ -58,4 +65,14 @@ export declare class SDInputElement extends LitElement {
 	 * Element that represents the primary input element.
 	 */
 	protected inputRef: Ref<HTMLInputElement>;
+}
+
+/**
+ * Determines whether the specified element is a {@link SDInputElement}.
+ * @param element Element to check.
+ * @returns `true` when the element is a {@link SDInputElement}; otherwise `false`.
+ */
+export function isSDInputElement(element: HTMLElement): element is SDInputElement {
+	// Helper function as mixins don't (easily) support `instanceof`.
+	return "__brand" in element && element.__brand === brand;
 }
