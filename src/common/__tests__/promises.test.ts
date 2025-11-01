d0f1e29e39ca277378566d5690e07c59c61b9724
Richard Herman <geekyeggo@users.noreply.github.com>
2025-11-01 17:04:53 +0000

    test: migrate to vitest

--- src/common/__tests__/promises.test.ts
+++ src/common/__tests__/promises.test.ts
@@ -1,3 +1,5 @@
+import { describe, expect, it } from "vitest";
+
 import { PromiseCompletionSource } from "../promises";
 
 describe("PromiseCompletionSource<T>", () => {
--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,13 @@ export default defineConfig({
 		environment: "node",
 		include: [
 			"src/api/**/__tests__/*.test.ts",
+			"src/common/__tests__/disposable.test.ts",
+			//"src/common/__tests__/enumerable.test.ts",
+			"src/common/__tests__/event-emitter.test.ts",
+			"src/common/__tests__/i18n.test.ts",
+			"src/common/__tests__/lazy.test.ts",
+			"src/common/__tests__/promises.test.ts",
+			"src/common/__tests__/utils.test.ts",
 			"src/common/events/**/__tests__/*.test.ts",
 			"src/common/logging/**/__tests__/*.test.ts",
 			"src/plugin/**/__tests__/*.test.ts",
