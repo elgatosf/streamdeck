b13c4a562844be46c898062a913631f65d06fe6c
Richard Herman <geekyeggo@users.noreply.github.com>
2025-11-01 15:21:46 +0000

    test: migrate to vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		environment: "node",
-		include: ["src/plugin/__tests__/connection.test.ts"],
+		include: [
+			"src/plugin/__tests__/connection.test.ts",
+			"src/plugin/common/__tests__/*",
+		],
 		setupFiles: [
 			"./tests/__setup__/global.ts",
 			"./tests/__setup__/ws.ts",
